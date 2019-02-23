67cadb9ffdff467563cbc9cbb2e0199bf727afe2
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-02-24 00:32:33 +0100

    deploy releases via circle ci

---
+++ .circleci/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir build
+GOOS=darwin GOARCH=386 go build -o build/semver_darwin_386 .
+GOOS=darwin GOARCH=amd64 go build -o build/semver_darwin_amd64 .
+GOOS=linux GOARCH=386 go build -o build/semver_linux_386 .
+GOOS=linux GOARCH=amd64 go build -o build/semver_linux_amd64 .
+GOOS=windows GOARCH=386 go build -o build/semver_windows_386.exe .
+GOOS=windows GOARCH=amd64 go build -o build/semver_windows_amd64.exe .
