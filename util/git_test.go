5af01f0d0cfe8b03702bd98ce2e24a1d6ddb1871
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-02-26 20:35:13 +0100

    test for CheckIfRepoIsClean

---
+++ util/git_test.go
@@ -0,0 +1,63 @@
+package util
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+)
+
+func setup(t *testing.T) (*git.Repository, string, func()) {
+	t.Log("init tmp git repo")
+
+	tmpFolder := "/tmp/github.com/meinto/semver/"
+	os.MkdirAll(tmpFolder, os.ModePerm)
+	r, err := git.PlainInit(tmpFolder, false)
+	if err != nil {
+		t.Error(err)
+	}
+
+	return r, tmpFolder, func() {
+		os.RemoveAll("/tmp/github.com/")
+		t.Log("finish")
+	}
+}
+
+func TestCheckIfRepoIsClean(t *testing.T) {
+	err := CheckIfRepoIsClean("/tmp")
+	t.Log(err)
+	if err == nil {
+		t.Error("should throw error because the repo is not initialized")
+	}
+
+	repo, repoPath, teardown := setup(t)
+	defer teardown()
+
+	filename := filepath.Join(repoPath, "example-git-file")
+	ioutil.WriteFile(filename, []byte("hello world!"), 0644)
+
+	err = CheckIfRepoIsClean(repoPath)
+	t.Log(err)
+	if err == nil {
+		t.Error("should throw error because there are uncomitted files")
+	}
+
+	w, _ := repo.Worktree()
+	w.Add(".")
+	w.Commit("test commit", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "Test User",
+			Email: "test@email.com",
+			When:  time.Now(),
+		},
+	})
+
+	err = CheckIfRepoIsClean(repoPath)
+	if err != nil {
+		t.Error(err)
+	}
+}
