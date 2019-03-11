5b124cee4f89a75c5a65e062a5fea1bbe016d1ee
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-11 22:34:44 +0100

    add flags on root command to tag a project with the current defined version and push this tag

--- cmd/internal/flags/root.go
+++ cmd/internal/flags/root.go
@@ -1,19 +1,65 @@
 package flags
 
 import (
+	"github.com/meinto/git-semver/util"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 type rootCmdFlagsType struct {
-	verbose bool
+	verbose         bool
+	repoPath        string
+	createTag       bool
+	push            bool
+	versionFile     string
+	versionFileType string
+	sshFilePath     string
 }
 
 var RootCmdFlags rootCmdFlagsType
 
 func (fs *rootCmdFlagsType) Init(cmd *cobra.Command) {
 	cmd.PersistentFlags().BoolVarP(&RootCmdFlags.verbose, "verbose", "v", false, "more logs")
+	cmd.Flags().StringVarP(&RootCmdFlags.repoPath, "path", "p", ".", "path to git repository")
+	cmd.Flags().BoolVarP(&RootCmdFlags.push, "push", "P", false, "push git tags")
+	cmd.Flags().BoolVarP(&RootCmdFlags.createTag, "tag", "T", false, "create a git tag")
+	cmd.Flags().StringVarP(&RootCmdFlags.versionFile, "versionFile", "f", "VERSION", "name of version file")
+	cmd.Flags().StringVarP(&RootCmdFlags.versionFileType, "versionFileType", "t", "raw", "type of version file (json, raw)")
+
+	defaultSSHFilePath, err := util.GetDefaultSSHFilePath()
+	util.LogOnError(err, RootCmdFlags.Verbose())
+	cmd.Flags().StringVar(&RootCmdFlags.sshFilePath, "sshFilePath", defaultSSHFilePath, "path to your ssh file")
+}
+
+func (fs *rootCmdFlagsType) PreRun(cmd *cobra.Command) {
+	bindViperFlag("versionFile", cmd.Flags().Lookup("versionFile"))
+	bindViperFlag("versionFileType", cmd.Flags().Lookup("versionFileType"))
 }
 
 func (fs *rootCmdFlagsType) Verbose() bool {
 	return fs.verbose
+}
+
+func (fs *rootCmdFlagsType) RepoPath() string {
+	return fs.repoPath
+}
+
+func (fs *rootCmdFlagsType) CreateTag() bool {
+	return fs.createTag
+}
+
+func (fs *rootCmdFlagsType) Push() bool {
+	return fs.push
+}
+
+func (fs *rootCmdFlagsType) VersionFile() string {
+	return viper.GetString("versionFile")
+}
+
+func (fs *rootCmdFlagsType) VersionFileFormat() string {
+	return viper.GetString("versionFileType")
+}
+
+func (fs *rootCmdFlagsType) SSHFilePath() string {
+	return fs.sshFilePath
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,22 +1,58 @@
 package cmd
 
 import (
+	"log"
 	"fmt"
+	"path/filepath"
+	"os"
+	"io/ioutil"
+	"github.com/meinto/git-semver/util" 
 
 	"github.com/gobuffalo/packr"
 	"github.com/meinto/git-semver/cmd/internal/flags"
 	"github.com/meinto/git-semver/cmd/internal"
-	"github.com/spf13/cobra"
+	"github.com/spf13/cobra" 
+	"github.com/pkg/errors" 
 )
 
 func init() {
-	flags.RootCmdFlags.Init(rootCmd)
+	flags.RootCmdFlags.Init(rootCmd) 
 }
 
 var rootCmd = &cobra.Command{
 	Use:   "semver",
 	Short: "standalone tool to version your gitlab repo with semver",
+	PreRun: func(cmd *cobra.Command, args []string) {
+		flags.RootCmdFlags.PreRun(cmd)
+	},
 	Run: func(cmd *cobra.Command, args []string) {
+
+		if flags.RootCmdFlags.CreateTag() {
+			gitRepoPath, err := filepath.Abs(flags.RootCmdFlags.RepoPath()) 
+			internal.LogFatalOnErr(errors.Wrap(err, "cannot resolve repo path"))
+
+			pathToVersionFile := internal.VersionFilePath(gitRepoPath, flags.RootCmdFlags.VersionFile())
+
+			_, err = os.Stat(pathToVersionFile) 
+			internal.LogFatalOnErr(errors.Wrap(err, "version file doesn't exist"))
+
+			versionFile, err := os.Open(pathToVersionFile)
+			internal.LogFatalOnErr(errors.Wrap(err, fmt.Sprintf("cannot read %s", flags.RootCmdFlags.VersionFile())))
+			defer versionFile.Close()
+
+			byteValue, err := ioutil.ReadAll(versionFile)
+			internal.LogFatalOnErr(errors.Wrap(err, "cannot read file"))
+			currentVersion := internal.GetVersion(flags.RootCmdFlags.VersionFileFormat(), byteValue)
+ 
+			util.MakeGitTag(gitRepoPath, currentVersion)
+		}
+
+		if flags.RootCmdFlags.Push() {
+			if err := util.Push(flags.RootCmdFlags.RepoPath(), flags.RootCmdFlags.SSHFilePath()); err != nil {
+				log.Fatalf("cannot push tag: %s", err.Error())
+			}
+		}
+
 		box := packr.NewBox("../buildAssets")
 		version, err := box.FindString("VERSION")
 		internal.LogFatalOnErr(err)
