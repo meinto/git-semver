709279f5362f3a78e34fae5cfa8a68dc389d5b10
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-12 14:40:57 +0100

    migrate all root flags

--- pkg/cli/cmd/root.go
+++ pkg/cli/cmd/root.go
@@ -1,49 +1,61 @@
 package cmd
 
 import (
-	"github.com/meinto/git-semver/git"
-	// "log"
 	"fmt"
-	// "path/filepath"
-	// "os"
-	// "io/ioutil"
-	// "github.com/meinto/git-semver/util" 
+	"log"
+	"os"
+
+	"github.com/meinto/git-semver/file"
+	"github.com/meinto/git-semver/git"
 
 	"github.com/gobuffalo/packr"
-	"github.com/meinto/git-semver/pkg/cli/cmd/internal/flags"
-	"github.com/meinto/git-semver/pkg/cli/cmd/internal"
-	"github.com/spf13/cobra" 
-	// "github.com/pkg/errors" 
+	"github.com/spf13/cobra"
+
+	// "github.com/pkg/errors"
 	"github.com/spf13/viper"
 )
 
-var rootCmdOptions struct {
-	// Author                string
-	GitPath               string
-	// UseBuiltInGitBindings bool
+var rootCmdFlags struct {
+	gitPath         string
+	verbose         bool
+	push            bool
+	createTag       bool
+	versionFile     string
+	versionFileType string
+	sshFilePath     string
 }
 
 func init() {
-	flags.RootCmdFlags.Init(rootCmd) 
-	rootCmd.PersistentFlags().StringVar(&rootCmdOptions.GitPath, "gitPath", "/usr/local/bin/git", "path to native git installation")
+	rootCmd.PersistentFlags().StringVar(&rootCmdFlags.gitPath, "gitPath", "/usr/local/bin/git", "path to native git installation")
+	rootCmd.PersistentFlags().BoolVarP(&rootCmdFlags.verbose, "verbose", "v", false, "more logs")
+	rootCmd.PersistentFlags().BoolVarP(&rootCmdFlags.push, "push", "P", false, "push git tags")
+	rootCmd.PersistentFlags().BoolVarP(&rootCmdFlags.createTag, "tag", "T", false, "create a git tag")
+	rootCmd.PersistentFlags().StringVarP(&rootCmdFlags.versionFile, "versionFile", "f", "VERSION", "name of version file")
+	rootCmd.PersistentFlags().StringVarP(&rootCmdFlags.versionFileType, "versionFileType", "t", "raw", "type of version file (json, raw)")
+
+	defaultSSHFilePath, err := file.GetDefaultSSHFilePath()
+	if err != nil {
+		log.Println(err)
+	}
+	rootCmd.PersistentFlags().StringVar(&rootCmdFlags.sshFilePath, "sshFilePath", defaultSSHFilePath, "path to your ssh file")
+
 	viper.BindPFlag("gitPath", rootCmd.PersistentFlags().Lookup("gitPath"))
+	viper.BindPFlag("versionFile", rootCmd.PersistentFlags().Lookup("versionFile"))
+	viper.BindPFlag("versionFileType", rootCmd.PersistentFlags().Lookup("versionFileType"))
 }
 
 var rootCmd = &cobra.Command{
 	Use:   "semver",
 	Short: "standalone tool to version your gitlab repo with semver",
-	PreRun: func(cmd *cobra.Command, args []string) {
-		flags.RootCmdFlags.PreRun(cmd)
-	},
 	Run: func(cmd *cobra.Command, args []string) {
 
 		// if flags.RootCmdFlags.CreateTag() {
-		// 	gitRepoPath, err := filepath.Abs(flags.RootCmdFlags.RepoPath()) 
+		// 	gitRepoPath, err := filepath.Abs(flags.RootCmdFlags.RepoPath())
 		// 	internal.LogFatalOnErr(errors.Wrap(err, "cannot resolve repo path"))
 
 		// 	pathToVersionFile := internal.VersionFilePath(gitRepoPath, flags.RootCmdFlags.VersionFile())
 
-		// 	_, err = os.Stat(pathToVersionFile) 
+		// 	_, err = os.Stat(pathToVersionFile)
 		// 	internal.LogFatalOnErr(errors.Wrap(err, "version file doesn't exist"))
 
 		// 	versionFile, err := os.Open(pathToVersionFile)
@@ -53,7 +65,7 @@ var rootCmd = &cobra.Command{
 		// 	byteValue, err := ioutil.ReadAll(versionFile)
 		// 	internal.LogFatalOnErr(errors.Wrap(err, "cannot read file"))
 		// 	currentVersion := internal.GetVersion(flags.RootCmdFlags.VersionFileFormat(), byteValue)
- 
+
 		// 	util.MakeGitTag(gitRepoPath, currentVersion)
 		// }
 
@@ -66,14 +78,19 @@ var rootCmd = &cobra.Command{
 		g := git.NewGitService(viper.GetString("gitPath"))
 		repoPath, _ := g.GitRepoPath()
 
-		box := packr.NewBox(repoPath+"/buildAssets")
+		box := packr.NewBox(repoPath + "/buildAssets")
 		version, err := box.FindString("VERSION")
-		internal.LogFatalOnErr(err)
+		if err != nil {
+			log.Fatal(err)
+		}
 		fmt.Printf("Version of git-semver: %s\n", version)
 	},
 }
 
 func Execute() {
 	err := rootCmd.Execute()
-	internal.LogFatalOnErr(err)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
 }
