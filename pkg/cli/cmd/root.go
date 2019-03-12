fe69bcedab5b89cecb74f28fa96e51046cac0c71
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-12 14:28:03 +0100

    fix displaying version in root cmd

--- pkg/cli/cmd/root.go
+++ pkg/cli/cmd/root.go
@@ -1,22 +1,32 @@
 package cmd
 
 import (
-	"log"
+	"github.com/meinto/git-semver/git"
+	// "log"
 	"fmt"
-	"path/filepath"
-	"os"
-	"io/ioutil"
-	"github.com/meinto/git-semver/util" 
+	// "path/filepath"
+	// "os"
+	// "io/ioutil"
+	// "github.com/meinto/git-semver/util" 
 
 	"github.com/gobuffalo/packr"
 	"github.com/meinto/git-semver/pkg/cli/cmd/internal/flags"
 	"github.com/meinto/git-semver/pkg/cli/cmd/internal"
 	"github.com/spf13/cobra" 
-	"github.com/pkg/errors" 
+	// "github.com/pkg/errors" 
+	"github.com/spf13/viper"
 )
+
+var rootCmdOptions struct {
+	// Author                string
+	GitPath               string
+	// UseBuiltInGitBindings bool
+}
 
 func init() {
 	flags.RootCmdFlags.Init(rootCmd) 
+	rootCmd.PersistentFlags().StringVar(&rootCmdOptions.GitPath, "gitPath", "/usr/local/bin/git", "path to native git installation")
+	viper.BindPFlag("gitPath", rootCmd.PersistentFlags().Lookup("gitPath"))
 }
 
 var rootCmd = &cobra.Command{
@@ -27,33 +37,36 @@ var rootCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 
-		if flags.RootCmdFlags.CreateTag() {
-			gitRepoPath, err := filepath.Abs(flags.RootCmdFlags.RepoPath()) 
-			internal.LogFatalOnErr(errors.Wrap(err, "cannot resolve repo path"))
+		// if flags.RootCmdFlags.CreateTag() {
+		// 	gitRepoPath, err := filepath.Abs(flags.RootCmdFlags.RepoPath()) 
+		// 	internal.LogFatalOnErr(errors.Wrap(err, "cannot resolve repo path"))
 
-			pathToVersionFile := internal.VersionFilePath(gitRepoPath, flags.RootCmdFlags.VersionFile())
+		// 	pathToVersionFile := internal.VersionFilePath(gitRepoPath, flags.RootCmdFlags.VersionFile())
 
-			_, err = os.Stat(pathToVersionFile) 
-			internal.LogFatalOnErr(errors.Wrap(err, "version file doesn't exist"))
+		// 	_, err = os.Stat(pathToVersionFile) 
+		// 	internal.LogFatalOnErr(errors.Wrap(err, "version file doesn't exist"))
 
-			versionFile, err := os.Open(pathToVersionFile)
-			internal.LogFatalOnErr(errors.Wrap(err, fmt.Sprintf("cannot read %s", flags.RootCmdFlags.VersionFile())))
-			defer versionFile.Close()
+		// 	versionFile, err := os.Open(pathToVersionFile)
+		// 	internal.LogFatalOnErr(errors.Wrap(err, fmt.Sprintf("cannot read %s", flags.RootCmdFlags.VersionFile())))
+		// 	defer versionFile.Close()
 
-			byteValue, err := ioutil.ReadAll(versionFile)
-			internal.LogFatalOnErr(errors.Wrap(err, "cannot read file"))
-			currentVersion := internal.GetVersion(flags.RootCmdFlags.VersionFileFormat(), byteValue)
+		// 	byteValue, err := ioutil.ReadAll(versionFile)
+		// 	internal.LogFatalOnErr(errors.Wrap(err, "cannot read file"))
+		// 	currentVersion := internal.GetVersion(flags.RootCmdFlags.VersionFileFormat(), byteValue)
  
-			util.MakeGitTag(gitRepoPath, currentVersion)
-		}
+		// 	util.MakeGitTag(gitRepoPath, currentVersion)
+		// }
 
-		if flags.RootCmdFlags.Push() {
-			if err := util.Push(flags.RootCmdFlags.RepoPath(), flags.RootCmdFlags.SSHFilePath()); err != nil {
-				log.Fatalf("cannot push tag: %s", err.Error())
-			}
-		}
+		// if flags.RootCmdFlags.Push() {
+		// 	if err := util.Push(flags.RootCmdFlags.RepoPath(), flags.RootCmdFlags.SSHFilePath()); err != nil {
+		// 		log.Fatalf("cannot push tag: %s", err.Error())
+		// 	}
+		// }
 
-		box := packr.NewBox("../buildAssets")
+		g := git.NewGitService(viper.GetString("gitPath"))
+		repoPath, _ := g.GitRepoPath()
+
+		box := packr.NewBox(repoPath+"/buildAssets")
 		version, err := box.FindString("VERSION")
 		internal.LogFatalOnErr(err)
 		fmt.Printf("Version of git-semver: %s\n", version)
