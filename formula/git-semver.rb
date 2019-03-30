15c121311433aacfd80b44fcf4e42c24381bb36e
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-30 17:16:08 +0100

    Brew formula update for git-semver version v5.0.0

--- formula/git-semver.rb
+++ formula/git-semver.rb
@@ -2,9 +2,9 @@
 class GitSemver < Formula
   desc ""
   homepage ""
-  url "https://github.com/meinto/git-semver/releases/download/v4.3.0/semver_4.3.0_darwin_x86_64.tar.gz"
-  version "4.3.0"
-  sha256 "cc49081633aa8d4ca397a8e6f6d570c92ef98da2f49148bea3f05f24be938977"
+  url "https://github.com/meinto/git-semver/releases/download/v5.0.0/semver_5.0.0_darwin_x86_64.tar.gz"
+  version "5.0.0"
+  sha256 "5712584ab226a23851e92dca9f7a367be6a604a49d4856b5ce752639290b6b7f"
   
   depends_on "git"
 
